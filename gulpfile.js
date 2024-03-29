0df0b468c93d26d21bddddd78272ef4621679ce8
Michael Grishchenko <mikigym19@gmail.com>
2024-03-30 00:27:33 +0300

    add clean, build, autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const browserSync = require('browser-sync');
 const uglify = require('gulp-uglify');
 const contat = require('gulp-concat');
 const rename = require('gulp-rename');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
 
 
 // обновление html
@@ -17,12 +19,25 @@ gulp.task('html', function() {
 gulp.task('scss', function() {
 	return gulp.src('app/scss/**/*.scss')
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 8 versions']
+		}))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(gulp.dest('app/css'))
 		.pipe(browserSync.reload({stream: true}))
 });
 
 
+gulp.task('css', function() {
+	return gulp.src([
+		'node_modules/normalize.css/normalize.css',
+		'node_modules/slick-carousel/slick/slick.css',
+		'node_modules/magnific-popup/dist/magnific-popup.css'
+	])
+	.pipe(contat('_libs.scss'))
+	.pipe(gulp.dest('app/scss'))
+	.pipe(browserSync.reload({stream: true}))
+});
 // обновление js файлов
 gulp.task('script', function() {
 	return gulp.src('app/js/*.js')
@@ -52,6 +67,30 @@ gulp.task('browser-sync', function() {
 });
 
 
+// на продакшин
+gulp.task('export', async function() {
+	const buildHtml = gulp.src('app/**/*.html')
+		.pipe(gulp.dest('dist'))
+
+	const buildCss = gulp.src('app/css/**/*.css')
+		.pipe(gulp.dest('dist/css'))
+
+	const buildJs = gulp.src('app/js/**/*.js')
+		.pipe(gulp.dest('dist/js'))
+
+	const buildFonts = gulp.src('app/fonts/**/*.*')
+		.pipe(gulp.dest('dist/fonts'))
+
+	const buildImg = gulp.src('app/img/**/*.*')
+		.pipe(gulp.dest('dist/img'))
+});
+
+
+gulp.task('clean', async function() {
+	del.sync('dist')
+});
+
+
 // автоматическая конвертация
 gulp.task('watch', function() {
 	gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
@@ -59,6 +98,8 @@ gulp.task('watch', function() {
 	gulp.watch('app/js/*.js', gulp.parallel('script'));
 });
 
+// del & build => вместе
+gulp.task('build', gulp.series('clean', 'export'));
 
 // дефолтный таск
-gulp.task('default', gulp.parallel('scss', 'js' ,'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('css', 'scss', 'js' ,'browser-sync', 'watch'));
